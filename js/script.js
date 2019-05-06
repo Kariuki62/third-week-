e1abc782778a2b2ddddf5acbca3f76753b745302
Kuro Arigami <kuro@localhost.localdomain>
2019-05-06 06:09:05 -0400

    Add hover effect

---
+++ js/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $("#design,#hocus2").click(function(){
+        $("#design").slideToggle();
+        $("#hocus2").slideToggle();
+    });
+ 
+    $("#dev,#hocus").click(function(){
+        $("#dev").slideToggle();
+        $("#hocus").slideToggle();
+    });
+    $("#product,#hocus3").click(function(){
+        $("#product").slideToggle();
+        $("#hocus3").slideToggle();
+    });
+    $("#workd").hover(function(){
+        $(this).css("background-color", "red");
+        }, function(){
+        $(this).css("background-color", "white");
+      });
+        $("#workc").hover(function(){
+          $(this).css("background-color", "red");
+          }, function(){
+          $(this).css("background-color", "white");
+      });
+        $("#workb").hover(function(){
+          $(this).css("background-color", "red");
+          }, function(){
+          $(this).css("background-color", "white");
+      });
+      $("#worka").hover(function(){
+        $(this).css("background-color", "red");
+        }, function(){
+        $(this).css("background-color", "white");
+     });
+     $("#worke").hover(function(){
+      $(this).css("background-color", "red");
+      }, function(){
+      $(this).css("background-color", "white");
+     });
+     $("#workf").hover(function(){
+      $(this).css("background-color", "red");
+      }, function(){
+      $(this).css("background-color", "white");
+     });
+     $("#workg").hover(function(){
+      $(this).css("background-color", "red");
+      }, function(){
+      $(this).css("background-color", "white");
+     });
+     $("#workh").hover(function(){
+      $(this).css("background-color", "red");
+      }, function(){
+      $(this).css("background-color", "white");
+     });
+ });
+ 
